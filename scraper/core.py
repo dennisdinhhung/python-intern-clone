01c67f8a17b310042e092f50539120452747d42c
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2022-06-13 15:14:12 +0700

    add: printing title and description

---
+++ scraper/core.py
@@ -0,0 +1,33 @@
+from re import A
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    
+}
+
+url = "https://vnexpress.net/giao-duc"
+
+req = requests.get(url)
+soup = BeautifulSoup(req.content, 'html.parser')
+
+# get all article
+articleTags = soup.findAll('article')
+
+obj = {}
+
+for article in articleTags:
+    # get the h3 tags in article
+    h3_tag = article.findAll('h3', class_='title-news')
+    if len(h3_tag) >= 1:
+        a_tag = h3_tag[0].find('a').string
+        print('Title: ' + a_tag)
+    
+    # get p tag with class='description'
+    p_tag = article.findAll('p', class_='description')
+    if len(p_tag) >= 1 and p_tag[0].find('a').string:
+        p_content = p_tag[0].find('a').string
+        print('Desc: ' + p_content)
+        
+# get title from class='title-news'
+# get desc from class='description'

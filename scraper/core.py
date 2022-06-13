8e53a949d40bb53d832d021325c721b27e00bb5c
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2022-06-13 17:36:23 +0700

    add: functions for title and desc

--- scraper/core.py
+++ scraper/core.py
@@ -2,10 +2,19 @@ from re import A
 import requests
 from bs4 import BeautifulSoup
 
-headers = {
-    
-}
+def getTitle(article):
+    h3_tag = article.findAll('h3', class_='title-news')
+    if len(h3_tag) >= 1:
+        a_tag = h3_tag[0].find('a').string
+        print('Title: ' + a_tag)
+        
+def getDesc(article):
+    p_tag = article.findAll('p', class_='description')
+    if len(p_tag) >= 1 and p_tag[0].find('a').string:
+        p_content = p_tag[0].find('a').string
+        print('Desc: ' + p_content)
 
+base_url = "https://vnexpress.net"
 url = "https://vnexpress.net/giao-duc"
 
 req = requests.get(url)
@@ -18,16 +27,10 @@ obj = {}
 
 for article in articleTags:
     # get the h3 tags in article
-    h3_tag = article.findAll('h3', class_='title-news')
-    if len(h3_tag) >= 1:
-        a_tag = h3_tag[0].find('a').string
-        print('Title: ' + a_tag)
+    getTitle()
     
     # get p tag with class='description'
-    p_tag = article.findAll('p', class_='description')
-    if len(p_tag) >= 1 and p_tag[0].find('a').string:
-        p_content = p_tag[0].find('a').string
-        print('Desc: ' + p_content)
+    getDesc()
         
 # get title from class='title-news'
 # get desc from class='description'

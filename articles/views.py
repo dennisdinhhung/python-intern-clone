df2a45b003b91337191cc2cd88b98b1e82a97b1d
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2022-09-06 16:44:53 +0700

    clean up

--- articles/views.py
+++ articles/views.py
@@ -19,10 +19,10 @@ class GetListNewsArticle(APIView):
         return [auth() for auth in self.authentication_classes]
     
     def get(self, request):
-        # query_params = request.query_params.dict()
-        # serializer = ArticleGetListSerializer(data=query_params)
-        # if not serializer.is_valid():
-        #     raise ValidationError(serializer.errors)
+        query_params = request.query_params.dict()
+        serializer = ArticleGetListSerializer(data=query_params)
+        if not serializer.is_valid():
+            raise ValidationError(serializer.errors)
         
         search = request.query_params.get('search') or ''
         newsarticle = Articles.objects.filter(Q(title__icontains=search)|
@@ -32,14 +32,6 @@ class GetListNewsArticle(APIView):
         page_obj = paginator.paginate_queryset(newsarticle, request)
         serializer = ArticleSerializer(page_obj, many=True)
         return paginator.get_paginated_response(serializer.data)
-    
-        # newsarticle = NewsArticles.objects.filter(Q(title__icontains=search)|
-        #                                           Q(description__icontains=search)|
-        #                                           Q(url__icontains=search))
-        # paginator = PageNumberPagination()
-        # page_obj = paginator.paginate_queryset(newsarticle, request)
-        # serializer = NewsSerializer(page_obj, many=True)
-        # return paginator.get_paginated_response(serializer.data)
 
 
     def post(self, request):

e65a3e9ec800c126d8f29381ed5fdc3625c2d079
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2022-09-13 15:11:34 +0700

    add: bulk_create(), missing: bulk_update()

--- articles/tasks.py
+++ articles/tasks.py
@@ -4,35 +4,36 @@ from bs4 import BeautifulSoup
 from django.conf import settings
 
 from articles.models import Articles
+from articles.serializers import ArticleCreateSerializer
 from project.celery import app as celery_app
 
 
 def get_title(article):
     """Return the title of the news"""
     h3_tag = article.find('h3', class_='title-news')
-    if h3_tag:
-        a_tag = h3_tag.find('a').string.strip()
-        return str(a_tag)
+    if not h3_tag:
+        return None
+    a_tag = h3_tag.find('a').string.strip()
+    return str(a_tag)
 
 
 def get_desc(article):
     """Return the description of the news"""
+    description = None
     p_tag = article.find('p', class_='description')
-    if p_tag:
-        test = p_tag.find('a').contents
-        for item in test:
-            if item.name == 'span':
-                continue
-            description = item
-        return str(description)
+    if not p_tag:
+        return None
+    description = p_tag.find('a')['title']
+    return str(description)
 
 
 def get_url(article):
     """Return the href/link of the news"""
     h3_tag = article.find('h3', class_='title-news')
-    if h3_tag:
-        a_tag = h3_tag.find('a')['href']
-        return str(a_tag)
+    if not h3_tag:
+        return None
+    a_tag = h3_tag.find('a')['href']
+    return str(a_tag)
 
 
 def save_articles(articles):
@@ -44,11 +45,8 @@ def save_articles(articles):
             Articles.objects.create(title=title, description=description, url=url)
 
 
-def get_articles(page):
-    base_url = f'{settings.VNEXPRESS_URL}/giao-duc-p{page}'
-    req = requests.get(base_url)
-    soup = BeautifulSoup(req.content, 'html.parser')
-    article_tags = soup.find_all('article')
+def get_articles(html_content):
+    article_tags = html_content.find_all('article')
     articles = []
     for article in article_tags:
         article_dict = {
@@ -59,18 +57,27 @@ def get_articles(page):
             continue
         if not article_dict['description']:
             continue
-        articles.append(article_dict)
+        serializer = ArticleCreateSerializer(data=article_dict)
+        if not serializer.is_valid():
+            continue
+        data = Articles(**serializer.validated_data)
+        articles.append(data)
+    return articles
 
-    if soup.find('a', class_='next-page'):
-        is_next_page = True
-    else:
-        is_next_page = False
-    return articles, is_next_page
+
+def is_next_page(html_content):
+    if html_content.find('a', class_='next-page'):
+        return True
+    return False
 
 
-@celery_app.task(name='tasks.article_scraper', bind=True, default_retry_delay=3, autoretry_for=(Exception,), retry_kwargs={'max_retries': 5})
+@celery_app.task(name='tasks.article_scraper', bind=True, default_retry_delay=3, autoretry_for=(Exception,),
+                 retry_kwargs={'max_retries': 5})
 def crawl(self, page=1):
-    articles, is_next_page = get_articles(page)
-    save_articles(articles)
-    if is_next_page:
+    base_url = f'{settings.VNEXPRESS_URL}/giao-duc-p{page}'
+    req = requests.get(base_url)
+    html_content = BeautifulSoup(req.content, 'html.parser')
+    articles = get_articles(html_content)
+    Articles.objects.bulk_create(articles)
+    if is_next_page(html_content):
         celery_app.send_task('tasks.article_scraper', (page + 1,))
